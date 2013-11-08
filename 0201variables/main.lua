7f3a5af45ad3b5cbb2c4d6673f2c5098038d3425
Ivan Vilches <bigboss@bigdesign.cl>
2013-11-08 01:42:12 -0400

    Tables

--- 0201variables/main.lua
+++ 0201variables/main.lua
@@ -21,3 +21,34 @@ local fooNess = fooBar .. three;
 print(fooNess);
 
 foo = nil;
+---------TABLES (ARRAYS)
+local t = {"foo", "bar"};
+print(t[2]); -- start from 1
+print(#t); -- lenght
+print(tostring(t)); -- name 
+local hash = {
+	foo = "foo",
+	bar = "bar"
+};
+
+local hash = {}; -- other method 
+hash.foo = "foo";
+hash.bar = "bar";
+
+print(hash["foo"]);
+print(hash.foo);
+
+local t = {
+	{"Foo", "Bar"}, -- multidimentional
+	{"hello", "there", "everyone"},
+	foo = {							--ascoaiative
+		bar = "nested barness!",
+		vintage = 75
+		}
+};
+
+print(t[1][2]); --Bar
+print(t[2][2] .. ", " .. t[2][3]); --there, everyone
+print(t.foo.vintage); -- asociative and return: 75
+
+t = nil;  -- garbage
